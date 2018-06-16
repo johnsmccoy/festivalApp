ccfd3685d669bffd563f77d20fa50d4c27839824
johnsmccoy <johnsmccoy@comcast.net>
2018-06-16 07:11:32 -0500

    login finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './login'
 
 class App extends Component {
+  setActiveUser = function(val){
+    if (val) {
+      localStorage.setItem("userId", val);
+      this.setState({
+        activeUser: val
+      })
+    } else {
+      localStorage.removeItem("userId");
+      this.setState({
+        activeUser: null
+      })
+    }
+  }.bind(this)
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Login setActiveUser={this.setActiveUser} />
       </div>
     );
   }
